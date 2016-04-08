f24f8c60e773f31be230da0966b065d8051f15ac
bailu <407840642@qq.com>
2016-04-08 11:16:11 +0800

    1111

--- app/src/main/java/com/example/administrator/mygittest/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygittest/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.administrator.mygittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = new Button(this);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "1", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
